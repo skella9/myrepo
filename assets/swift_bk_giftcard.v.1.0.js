63e4984a7cae4856d3c13f90b118e0adcc4ef4a8
--unset <bullseyedev1@gmail.com>
2022-06-30 17:51:18 -1000

    live

--- assets/swift_bk_giftcard.v.1.0.js
+++ assets/swift_bk_giftcard.v.1.0.js
@@ -1,1 +1,47 @@
-!function(){var a={qrCode:"qr-code",printButton:"print-gift-card",giftCardCode:"gift-card-code-digits"},b=document.getElementById(a.qrCode);new QRCode(b,{text:b.getAttribute("data-identifier"),width:120,height:120}),document.getElementById(a.printButton).addEventListener("click",function(){window.print()},!1),document.getElementById(a.giftCardCode).addEventListener("click",function(d){var b=d.target,a="";if(document.body.createTextRange)(a=document.body.createTextRange()).moveToElementText(b),a.select();else if(window.getSelection){var c=window.getSelection();(a=document.createRange()).selectNodeContents(b),c.removeAllRanges(),c.addRange(a)}},!1)}()
+/**
+ * v.1.0
+ *
+ * Gift Card Template Script
+ * ------------------------------------------------------------------------------
+ * A file that contains scripts highly couple code to the Gift Card template.
+ */
+
+(function () {
+  var config = {
+    qrCode: 'qr-code',
+    printButton: 'print-gift-card',
+    giftCardCode: 'gift-card-code-digits' };
+
+
+  var qrCode = document.getElementById(config.qrCode);
+
+  new QRCode(qrCode, {
+    text: qrCode.getAttribute('data-identifier'),
+    width: 120,
+    height: 120 });
+
+
+  document.getElementById(config.printButton).addEventListener('click', function () {
+    window.print();
+  }, false);
+
+  function selectText(evt) {
+    var text = evt.target;
+    var range = '';
+
+    if (document.body.createTextRange) {
+      range = document.body.createTextRange();
+      range.moveToElementText(text);
+      range.select();
+    } else if (window.getSelection) {
+      var selection = window.getSelection();
+      range = document.createRange();
+      range.selectNodeContents(text);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  }
+
+  // Auto-select gift card code on click, based on ID passed to the function
+  document.getElementById(config.giftCardCode).addEventListener('click', selectText, false);
+})();
